f7f96c0ad6879036d6cc5860a7d8c18065bdb899
CYB101NAME <CYB101EMAIL>
2026-05-30 23:21:43 -0500

    add logistic_regression files to the folder

---
+++ scaling.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+class StandardScaler:
+
+    def fit(self, X):
+        self.mean = X.mean(axis=0)
+        self.std = X.std(axis=0)
+
+    def transform(self, X):
+        return (X - self.mean) / self.std
+
+    def fit_transform(self, X):
+        self.fit(X)
+        return self.transform(X)
